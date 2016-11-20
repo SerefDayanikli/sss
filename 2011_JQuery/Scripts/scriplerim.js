f25f31ec2a398d84375fabff572627affa3ebd6a
SerefDayanikli <seref_day@hotmail.com>
2016-11-20 15:00:44 +0200

    ılk yukleme

---
+++ 2011_JQuery/Scripts/scriplerim.js
@@ -0,0 +1,30 @@
+﻿<script>
+
+    $().ready(function () {
+        function topla(x, y) {
+            return x + y;
+        };
+        topla(5, 8);
+        $.SayilariTopla(3, 5);
+        $.islemler = {
+            topla: function (x, y) {
+                return x + y;
+            },
+            cikar: function (x, y) {
+                return x - y;
+            },
+            carp: function (x, y) {
+                return x * y;
+
+            },
+            bol: function () {
+                return x / y;
+            }
+                
+        }
+
+
+           
+           
+    });
+</script>
